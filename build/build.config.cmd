ba9dcc9628005dc6f710fa2b8339b7728b6edd47
Grigore Stefan <g_stefan@yahoo.com>
2021-12-01 03:24:01 +0200

    Update

--- build/build.config.cmd
+++ build/build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-sysinternals
-set PRODUCT_VERSION=2020.07.15
+set PRODUCT_VERSION=2021.10.26
 set PRODUCT_BASE=sysinternals
